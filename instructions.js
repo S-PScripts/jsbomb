27f17c3de9b62480a907841f4b246e558b347f89
S-PScripts <ts2021me@outlook.com>
2026-04-06 14:37:02 +0100

    Create instructions.js

---
+++ instructions.js
@@ -0,0 +1,25 @@
+// Instructions
+// How to get your scratch X-CSRFToken
+// 1. Comment something
+// 2. Go to inspect element 
+// 3. Go to the network section
+// 4. Find /session
+// 5. Copy the token
+// Note: This X-CSRFToken will change so you'll need to do this every time you're JSBOMBing.
+
+// How to get your scratch x-token
+// 1. Comment something
+// 2. Go to scratch.mit.edu/session
+// 3. View the page's source
+// 4. Copy the x-token
+// Note: This x-token will always stay the same
+
+// How to get the project.json
+// 1. Download the project you want to use
+// 2. Convert it to a zip 
+// 3. Open the zip
+// 4. Open the folder created by the zip
+// 5. Open the json file.
+// 6. Copy the text in it.
+
+// JSBOMBING IS DEAD THERE IS NO POINT USING THIS
