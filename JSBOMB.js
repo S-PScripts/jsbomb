9f80b6785a0adf593e0647ec2ca22fd63f088dfd
S-PScripts <ts2021me@outlook.com>
2026-04-06 14:36:43 +0100

    Update JSBOMB.js

--- JSBOMB.js
+++ JSBOMB.js
@@ -1,16 +1,12 @@
 // JSBOMBING IS DEAD THERE IS NO POINT USING THIS
-// JSBOMBING IS DEAD THERE IS NO POINT USING THIS
-// JSBOMBING IS DEAD THERE IS NO POINT USING THIS
 
 // You need to do something different to find x-token, but it's similar.
-// If you need more help, check my YT video on my YT channel.
-// If you still can't figure it out, you're a skid.
-// I didn't the JSBOMB here, only on my scripts repo, but I won't delete it now. Just don't use it.
+// If you can't figure it out, you're a skid.
 
 // This Scratch Javascript remix bomb was made by Derv, not me (TS2021).
 // To use this, execute the code below into your console (Inspect Element - Console section)
 
-// You will need to include the project.json (check the bottom of this page to see how to get this)
+// You will need to include the project.json (check instructions.js)
 
 // Settings
 var e = 1 // This is the project id of the starting project
@@ -108,36 +104,4 @@ jsbomb = async function() {
 
 jsbomb();
 
-// JSBOMBING IS DEAD THERE IS NO POINT USING THIS
-// JSBOMBING IS DEAD THERE IS NO POINT USING THIS
-// JSBOMBING IS DEAD THERE IS NO POINT USING THIS
-
-// YOU CAN DELETE EVERYTHING BELOW THIS LINE
-
-// Instructions
-// How to get your scratch X-CSRFToken
-// 1. Comment something
-// 2. Go to inspect element 
-// 3. Go to the network section
-// 4. Find /session
-// 5. Copy the token
-// Note: This X-CSRFToken will change so you'll need to do this every time you're JSBOMBing.
-
-// How to get your scratch x-token
-// 1. Comment something
-// 2. Go to scratch.mit.edu/session
-// 3. View the page's source
-// 4. Copy the x-token
-// Note: This x-token will always stay the same
-
-// How to get the project.json
-// 1. Download the project you want to use
-// 2. Convert it to a zip 
-// 3. Open the zip
-// 4. Open the folder created by the zip
-// 5. Open the json file.
-// 6. Copy the text in it.
-
-// JSBOMBING IS DEAD THERE IS NO POINT USING THIS
-// JSBOMBING IS DEAD THERE IS NO POINT USING THIS
 // JSBOMBING IS DEAD THERE IS NO POINT USING THIS
